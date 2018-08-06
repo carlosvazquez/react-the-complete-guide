5b8f7711c260c84d323d4944b4a2384032901caf
Carlos Vazquez <carlosvazquezmurillo@gmail.com>
2018-08-05 23:27:10 -0500

    Advances in state changes

--- src/App.js
+++ src/App.js
@@ -10,21 +10,29 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     value: 1,
-    person: [
-      {id:1,name: 'John', age: 34 },
-      {id:2,name: 'Karen', age: 24 },
-      {id:3,name: 'Bob', age: 45 }
+    persons: [
+      {id:1, name: 'John', age: 34 },
+      {id:2, name: 'Karen', age: 24 },
+      {id:3, name: 'Bob', age: 45 }
     ],
     showPersons: false
   }
-  nameChangeHandler = (event) => {
-    this.setState({
-      person: [
-        {name: 'Max', age: 34 },
-        {name: event.target.value, age: 29 },
-        {name: 'Roberto', age: 45 }
-      ]
+  nameChangeHandler = (event, id) => {
+    // findIndex recibe una funcion
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+    // copy 
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name = event.target.value;
+    // sacamos copy
+    const persons = [...this.state.persons];
+    // con el index buscamos el elemento y lo reasignamos
+    persons[personIndex] = person; 
+    this.setState({ persons: persons });
   }
   togglePersonHandler = () => {
     const doesShow = this.state.showPersons;
@@ -33,7 +41,7 @@ class App extends Component {
     });
   }
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.person;
+    const persons = this.state.persons;
     persons.splice(personIndex, 1);
     this.setState({
       persons: persons
@@ -48,12 +56,14 @@ class App extends Component {
     if(this.state.showPersons) {
       person = (
       <div>
-        { this.state.person.map((person, index) => {
+        { this.state.persons.map((person, index) => {
           return <Person
           click={() => this.deletePersonHandler(index) }
           name={ person.name }
           key={ person.id }
-          age={ person.age } />
+          age={ person.age }
+          changed={(event)=> this.nameChangeHandler(event, person.id) }
+          />
         })}
       </div>
       );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,7 +6,8 @@ const person = (props) => {
   }
   return (
   <div style={orangeblock} className="Person">
-    <h3>I'm { Math.floor(Math.random() * 30) } years old</h3>
+    <h3>I'm { props.name } with { Math.floor(Math.random() * 30) } years old</h3>
+    {/* cuando hagas click, ejecutas la propiedad click que en este caso es un metodo */}
     <button onClick={ props.click}>Borrar</button>
     <input type="text" onChange={ props.changed } value={ props.name }/>
     <p>{ props.children }</p>
